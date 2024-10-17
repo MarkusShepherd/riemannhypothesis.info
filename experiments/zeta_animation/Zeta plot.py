d3daf70b4136cdfe52779a0b42f018a65ba4f81a
Markus Schepke <markus.schepke@wolt.com>
2024-10-17 22:16:23 +0300

    Added Zeta plot.py

---
+++ experiments/zeta_animation/Zeta plot.py
@@ -0,0 +1,51 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import jupyter_black
+import numpy as np
+from matplotlib import pyplot as plt
+from mpmath import zeta
+from zeta_animation import make_faded_colors, plot_complex, prepare_axes
+
+jupyter_black.load()
+
+# %%
+critical_line = np.array(
+    [complex(0.5, t) for t in np.linspace(start=0, stop=200, num=20_000)]
+)
+zetas = np.array([zeta(z) for z in critical_line])
+colors = tuple(
+    make_faded_colors(
+        base_color="orange",
+        fade_steps=1000,
+        min_opacity=0.2,
+    )
+)
+
+# %%
+fig, ax = prepare_axes(
+    background_color="black",
+    axis_color="darkgrey",
+    grid=False,
+    figsize=(38.4, 21.6),
+    dpi=100,
+)
+plot_complex(
+    values=zetas,
+    colors=colors,
+    ax=ax,
+)
+plt.show()
